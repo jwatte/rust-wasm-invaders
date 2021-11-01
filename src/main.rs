29980830c1371c311a147713a8be1062a04aff7a
jwatte <jwatte@gmail.com>
2021-11-01 14:47:07 -0700

    refactor how sprites load

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use macroquad::prelude::*;
 use std::fmt::format;
+use std::borrow::Cow;
+use std::borrow::Borrow;
+
+const ASSUMED_SCREEN_WIDTH: i32 = 2400;
 
 #[macroquad::main("Invaders")]
 async fn main() {
@@ -14,50 +18,70 @@ async fn main() {
         let (left, top, width, height) = letterbox(sw, sh);
 
         {
+            //  draw lives
+            let pspr = &assets.sprites[0];
+            for n in 0..state.lives {
+                let dtp = DrawTextureParams {
+                    dest_size: Some(vec2(width * pspr.f_w * 0.45, width * pspr.f_h * 0.45)),
+                    ..Default::default()
+                };
+                draw_texture_ex(
+                    pspr.texture,
+                    left + width * (n as f32) * pspr.f_w * 0.5,
+                    top + height - width * pspr.f_w * 0.5,
+                    LIGHTGRAY,
+                    dtp,
+                );
+            }
+
             //  draw player
             let dtp = DrawTextureParams {
-                dest_size: Some(vec2(width * 0.04, width * 0.04)),
+                dest_size: Some(vec2(width * pspr.f_w, width * pspr.f_h)),
                 ..Default::default()
             };
             draw_texture_ex(
-                assets.player_cannon,
-                left + state.player_pos_fr * width - width * 0.02,
-                top + height * 0.94 - width * 0.02,
+                pspr.texture,
+                left + state.player_pos_fr * width - width * pspr.f_w * 0.5,
+                top + height * 0.94 - width * pspr.f_w * 0.5,
                 WHITE,
                 dtp,
             );
         }
 
-        //  draw lives
-        for n in 0..state.lives {
-            let dtp = DrawTextureParams {
-                dest_size: Some(vec2(width * 0.018, width * 0.018)),
+        //  draw aliens
+        for alien in state.aliens.iter() {
+            let asp = &assets.sprites[alien.index];
+            let adtp = DrawTextureParams {
+                dest_size: Some(vec2(width * asp.f_w, width * asp.f_h)),
+                rotation: alien.phase.sin()*0.1,
+                pivot: Some(vec2(left+width*alien.xpos, top + width*alien.ypos)),
                 ..Default::default()
             };
             draw_texture_ex(
-                assets.player_cannon,
-                left + width * (n as f32) * 0.02,
-                top + height - width * 0.02,
-                LIGHTGRAY,
-                dtp,
-            );
+                asp.texture,
+                left + width * alien.xpos - width * asp.f_w * 0.5,
+                top + width * alien.ypos - width * asp.f_h * 0.5,
+                WHITE,
+                adtp
+            )
         }
 
-        //  draw bullets
-        for bullet in state.bullets.iter() {
-            draw_texture_ex(
-                assets.player_laser_bolt,
-                left + width * (bullet.xpos - 0.004),
-                top + width * bullet.ypos,
-                WHITE,
-                DrawTextureParams {
-                    dest_size: Some(vec2(width * 0.008, width * 0.024)),
-                    ..Default::default()
-                },
-            );
-            //let bdisp = format!("{},{}", bullet.xpos, bullet.ypos);
-            //draw_text(&bdisp, left, top + height * 0.1, height * 0.02, RED);
-        }
+        {
+            let bspr = &assets.sprites[1];
+            //  draw bullets
+            for bullet in state.bullets.iter() {
+                draw_texture_ex(
+                    bspr.texture,
+                    left + width * (bullet.xpos - bspr.f_w * 0.5),
+                    top + width * bullet.ypos,
+                    WHITE,
+                    DrawTextureParams {
+                        dest_size: Some(vec2(width * bspr.f_w, width * bspr.f_h)),
+                        ..Default::default()
+                    },
+                );
+            }
+        }   
 
         //  draw masking bars
         draw_rectangle(0.0, 0.0, left, sh, BLACK);
@@ -75,9 +99,14 @@ async fn main() {
             WHITE,
         );
 
+        if state.reset_countdown > 0.0 {
+            let ctdisp = format!("Countdown: {:.1}", state.reset_countdown);
+            draw_text(&ctdisp, left + width * 0.3, top + height * 0.2, height * 0.04, WHITE);
+        }
+
         next_frame().await;
 
-        update_state(&mut state);
+        update_state(&mut state, &assets);
     }
 }
 
@@ -89,7 +118,7 @@ struct Bullet {
 }
 
 struct Alien {
-    sprite: Texture2D,
+    index: usize,
     xpos: f32,
     ypos: f32,
     phase: f32,
@@ -133,13 +162,18 @@ fn new_game_state() -> State {
     };
 }
 
-fn reset_level(_level: i32, state: &mut State) {
+fn reset_level(_level: i32, state: &mut State, assets: &Assets) {
     state.bullets = Vec::new();
     state.aliens = Vec::new();
     state.player_pos_fr = 0.48;
+
+    state.aliens.push(Alien { index: IX_ALIEN_1, xpos: 0.1, ypos: 0.1, phase: 0.0, dead: false, dead_timer: 0.0 });
+    state.aliens.push(Alien { index: IX_ALIEN_2, xpos: 0.15, ypos: 0.1, phase: 0.0, dead: false, dead_timer: 0.0 });
+    state.aliens.push(Alien { index: IX_ALIEN_1, xpos: 0.2, ypos: 0.1, phase: 0.0, dead: false, dead_timer: 0.0 });
+    state.aliens.push(Alien { index: IX_ALIEN_2, xpos: 0.25, ypos: 0.1, phase: 0.0, dead: false, dead_timer: 0.0 });
 }
 
-fn update_state(state: &mut State) {
+fn update_state(state: &mut State, assets: &Assets) {
     let delta_time = get_frame_time();
 
     //  evolve timers
@@ -147,32 +181,40 @@ fn update_state(state: &mut State) {
         state.time_to_fire -= delta_time;
     }
 
-    //  evolve inputs
-    if is_key_down(KeyCode::Right) {
-        state.player_pos_fr += delta_time * state.player_speed;
-    }
-    if is_key_down(KeyCode::Left) {
-        state.player_pos_fr -= delta_time * state.player_speed;
-    }
-
     if state.reset_countdown > 0.0 {
         state.reset_countdown -= delta_time;
         if state.reset_countdown <= 0.0 {
             state.current_level += 1;
-            reset_level(state.current_level, state);
+            reset_level(state.current_level, state, assets);
         }
     } else {
+        //  evolve inputs
+        if is_key_down(KeyCode::Right) {
+            state.player_pos_fr += delta_time * state.player_speed;
+        }
+        if is_key_down(KeyCode::Left) {
+            state.player_pos_fr -= delta_time * state.player_speed;
+        }
+
         state.player_pos_fr = clamp(state.player_pos_fr, 0.02, 0.98);
         if is_key_down(KeyCode::Space) {
             if state.time_to_fire <= 0.0 {
                 state.time_to_fire = state.firing_duration;
                 state.bullets.push(Bullet {
                     xpos: state.player_pos_fr,
-                    ypos: 1.33 * 0.94 - 0.03,
+                    ypos: 1.33 * 0.94 - 0.01,
                     velocity: state.fire_velocity,
                     dead: false,
                 })
             }
+        }
+    }
+
+    //  evolve aliens
+    for alien in state.aliens.iter_mut() {
+        alien.phase += delta_time * 2.4;
+        if alien.phase >= 3.1416 {
+            alien.phase -= 6.2832;
         }
     }
 
@@ -194,24 +236,87 @@ fn update_state(state: &mut State) {
     }
 }
 
+
+
+struct Sprite {
+    index: usize,
+    filename: Cow<'static, str>,
+    width_pix: i32,
+    height_pix: i32,
+    scale: f32,
+}
+
+struct LoadedSprite {
+    sprite: &'static Sprite,
+    texture: Texture2D,
+    f_w: f32,
+    f_h: f32,
+}
+
 struct Assets {
-    player_cannon: Texture2D,
-    player_laser_bolt: Texture2D,
-    alien_1: Texture2D,
-    alien_2: Texture2D,
+    sprites: Vec<LoadedSprite>,
+}
+
+const fn b_str(s: &'static str) -> Cow<'static, str> {
+    return Cow::Borrowed(s);
+}
+
+const IX_PLAYER : usize = 0;
+const IX_LASER : usize = 1;
+const IX_ALIEN_1 : usize = 2;
+const IX_ALIEN_2 : usize = 3;
+
+static PLAYER_SPRITE : Sprite = Sprite {
+    index: IX_PLAYER,
+    filename: b_str("data/playerShip3_blue.png"),
+    width_pix: 95,
+    height_pix: 75,
+    scale: 1.0,
+};
+
+static LASER_SPRITE : Sprite = Sprite {
+    index: IX_LASER,
+    filename: b_str("data/Lasers/laserBlue03.png"),
+    width_pix: 9,
+    height_pix: 37,
+    scale: 1.0,
+};
+
+static ENEMY1_SPRITE : Sprite = Sprite {
+    index: IX_ALIEN_1,
+    filename: b_str("data/Enemies/enemyGreen3.png"),
+    width_pix: 103,
+    height_pix: 84,
+    scale: 1.0,
+};
+
+static ENEMY2_SPRITE : Sprite = Sprite {
+    index: IX_ALIEN_2,
+    filename: b_str("data/Enemies/enemyRed1.png"),
+    width_pix: 93,
+    height_pix: 84,
+    scale: 1.0,
+};
+
+async fn load_sprite(spr : &'static Sprite) -> LoadedSprite {
+    let texture = load_texture(spr.filename.borrow()).await.unwrap();
+    return LoadedSprite{
+        sprite: spr,
+        texture: texture,
+        f_w: (spr.width_pix as f32) / (ASSUMED_SCREEN_WIDTH as f32) * spr.scale,
+        f_h: (spr.height_pix as f32) / (ASSUMED_SCREEN_WIDTH as f32) * spr.scale,
+    };
 }
 
 async fn load_assets() -> Assets {
-    let player_cannon = load_texture("data/playerShip3_blue.png").await.unwrap();
-    let player_laser_bolt = load_texture("data/Lasers/laserBlue03.png").await.unwrap();
-    let alien_1 = load_texture("data/Enemies/enemyGreen3.png").await.unwrap();
-    let alien_2 = load_texture("data/Enemies/enemyRed1.png").await.unwrap();
-    return Assets {
-        player_cannon: player_cannon,
-        player_laser_bolt: player_laser_bolt,
-        alien_1: alien_1,
-        alien_2: alien_2,
-    };
+    let mut vec: Vec<LoadedSprite> = Vec::new();
+    vec.push(load_sprite(&PLAYER_SPRITE).await);
+    vec.push(load_sprite(&LASER_SPRITE).await);
+    vec.push(load_sprite(&ENEMY1_SPRITE).await);
+    vec.push(load_sprite(&ENEMY2_SPRITE).await);
+    return Assets{
+        sprites: vec,
+    }
 }
 
 //  Given width/height, which part of the screen do we draw to?
